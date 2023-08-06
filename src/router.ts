b11f3c95a83432308fa5152317df0ea73f982fc3
zhangjunpeng <zhangjunpeng@58.com>
2023-08-06 13:04:52 +0800

    feat: 没有改什么

--- src/router.ts
+++ src/router.ts
@@ -1,30 +1,29 @@
-import Vue from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import ShallowReactiveCom from './pages/shallowReactive.vue'
 import WatcheffectCom from './pages/watcheffect.vue'
 import WXh5 from './pages/weixinh5.vue'
 
 const routes = [
-  {
-    path: '/shallowReactive',
-    name: 'shallowReactive',
-    component: ShallowReactiveCom,
-  },
-  {
-    path: '/watcheffect',
-    name: 'watcheffect',
-    component: WatcheffectCom,
-  },
-  {
-    path: '/wxh5',
-    name: 'wxh5',
-    component: WXh5,
-  },
+    {
+        path: '/shallowReactive',
+        name: 'shallowReactive',
+        component: ShallowReactiveCom
+    },
+    {
+        path: '/watcheffect',
+        name: 'watcheffect',
+        component: WatcheffectCom
+    },
+    {
+        path: '/wxh5',
+        name: 'wxh5',
+        component: WXh5
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: routes
+    history: createWebHistory(),
+    routes: routes
 })
 
-export default router;
+export default router
